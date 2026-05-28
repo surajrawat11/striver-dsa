2f6cc23ea8b063189b6e62e02ac90fd078f455b3
surajrawat11 <isurajrawat23@gmail.com>
2026-05-28 21:17:18 +0530

    Added Step1_Basics files (helloworld.cpp, datatypes.cpp, input-output.cpp)

---
+++ Step1_Basics/datatypes.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>   //All header files included in this 
+
+using namespace std;
+int main(){
+
+    //int 
+    int x = 10;   // Range -2,147,483,648 to 2,147,483,647
+    //long
+    long a = 14;
+    //long long
+    long long b = 2099708;
+    
+
+    //float 
+    float y = 5.244;
+
+    double z = 6.35253;
+
+    //strings
+    string s1,s2;
+    cout<<"enter an string: ";
+    cin>>s1>>s2;
+    cout<<"The string is : " <<s1<<" "<<s2;
+
+    return 0;
+}
---
+++ Step1_Basics/input-output.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"enter the value of x: ";
+    cin>>x;
+    cout<<"Value of x is:" <<x;
+    return 0;
+}
