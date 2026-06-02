026d65c657fd2a3c473898cb94938086deabfe10
surajrawat11 <isurajrawat23@gmail.com>
2026-06-02 21:06:51 +0530

    Add Step1 basics and Step2 sorting files

---
+++ Step1_Basics/functions()/function.cpp
@@ -0,0 +1,15 @@
+/* Functions are set of code which performs something for you
+Functions are used to modularise code
+Functions are used to increase readability
+Functions are used to use same code multiple times*/
+//void
+//return 
+//parameterised
+//non parameterised
+
+#include<iostream>
+using namespace std;
+int main(){
+    
+    return 0;
+}
---
+++ Step1_Basics/pattern_printing/pattern1.cpp
@@ -0,0 +1,13 @@
+/*Take an integer N as input to define the size of the square.
+Use a loop from 0 to N-1 to represent each row.
+Inside that loop, use another loop from 0 to N-1 to print stars in the current row.
+Print "* " during each inner loop iteration to form the row.
+After each inner loop completes, move to the next line.*/
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    
+    return 0;
+}
